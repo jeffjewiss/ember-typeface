bd87e11ab4daaece7e16a4a0f5eae95fb6d3c311
Jeff Jewiss <jeff@jeffjewiss.com>
2017-11-22 13:35:48 +0000

    Initial idea for checking configured typefaces

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 /* eslint-env node */
 'use strict';
 
+const merge = require('merge')
+const detectInstalled = require('detect-intsalled')
+const typefaceList = require('./lib/typefaces')
+
 module.exports = {
-  name: 'ember-typeface'
+  name: 'ember-typeface',
+
+  included (app) {
+    this.options = merge.recursive({}, {
+      typefaces: [
+      ]
+    }, this._getAddonOptions(app).typefaceOptions)
+
+    if (!this.options.typefaces.length) {
+      return;
+    }
+
+    this._checkTypefaces()
+  },
+
+  _getAddonOptions (app) {
+    return (this.parent && this.parent.options) || (app && app.options) || {}
+  },
+
+  _checkTypefaces () {
+    this.options.typefaces.forEach((typeface) => {
+      if (!typefaceList.includes(typeface)) {
+        throw new Error(`The font '${typeface}' is not supported. Please chose a font from the available list.`)
+      }
+
+      if (!detectInstalled.sync(`typeface-${typeface}`, { local: true })) {
+        throw new Error(`The font package 'typeface-${typeface}' is not installed. Please add it to your project with NPM or Yarn.`)
+      }
+    })
+  }
 };
