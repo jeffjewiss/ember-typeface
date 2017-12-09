def20e4f6c3e34bd6970a5a66e8b72204677f307
Jeff Jewiss <jeff@jeffjewiss.com>
2017-12-09 00:18:21 +0000

    Add version checker to ensure ember-cli >=2.16

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const merge = require('merge')
 const globby = require('globby')
 const detectInstalled = require('detect-installed')
 const typefaceList = require('./lib/typefaces')
+const VersionChecker = require('ember-cli-version-checker')
 
 module.exports = {
   name: 'ember-typeface',
+
+  init () {
+    let checker = new VersionChecker(this);
+    let assertMessage = 'To use ember-typeface you must have ember-cli 2.16 or above.'
+
+    checker
+      .for('ember-cli')
+      .assertAbove('2.16.0', assertMessage);
+  },
 
   included (app) {
     this.options = merge.recursive({}, {
