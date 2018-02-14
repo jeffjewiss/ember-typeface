76f8317f5ce7e7c6a1218be36878637f971a414f
Jeff Jewiss <jeff@jeffjewiss.com>
2018-02-14 12:20:03 +0000

    upgrade to ember 3.0.0

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,12 +1,12 @@
-import Ember from 'ember';
+import Router from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+const EmberRouter = Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
-Router.map(function() {
+EmberRouter.map(function() {
 });
 
-export default Router;
+export default EmberRouter;
